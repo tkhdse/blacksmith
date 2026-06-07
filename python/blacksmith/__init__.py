ac0416bb76d3520bcd30f8d36ceb46c7173b6fb3
Tanish Khadse <tanishk6201@gmail.com>
2026-06-06 23:52:58 -0500

    set up simple python->C++ binding

--- python/blacksmith/__init__.py
+++ python/blacksmith/__init__.py
@@ -2,9 +2,17 @@ import blacksmith
 import torch
 import torch.nn as nn
 
+import example
+
+# blacksmith API
+
 def compile(model: nn.Module):
     module = torch.export.export(model(), (torch.randn(100, 8), ))
     fx_out = module.run_decompositions().graph
+
+    ret = example.add(1,2)
+    print(ret) # 3
+
     print(fx_out)
     # serialized_nodes = 
     # fx_graph = blacksmith.compile(serialized_nodes)
