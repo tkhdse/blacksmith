9593241fa905f9038296b6036013c284bee15a74
Tanish Khadse <tanishk6201@gmail.com>
2026-07-11 19:17:57 -0700

    metal-cpp attempt to init Library, Function, CommandQueue (used NS)

--- test/metal/kernels/invoke.cpp
+++ test/metal/kernels/invoke.cpp
@@ -10,9 +10,22 @@
 
 int main() {
     MTL::Device* device = MTL::CreateSystemDefaultDevice();
-    MTL::Library* lib;
-    MTL::Function* func;
 
+    NS::Error* err = nullptr;
+    MTL::Library* lib = device->newLibrary(NS::String::string("defaultLib", NS::UTF8StringEncoding), &err);
+
+    if (!lib) {
+        std::cerr << "failed to create: lib" << std::endl;
+    }
+
+    MTL::Function* func = lib->newFunction(NS::String::string("mm_", NS::UTF8StringEncoding)); //"mm_"
+
+    if (!func) {
+        std::cerr << "failed to link: func" << std::endl;
+    }
+
+
+    MTL::CommandQueue* cq = device->newCommandQueue();
 
     std::cout << "invoked test metal pipeline" << std::endl;
 
